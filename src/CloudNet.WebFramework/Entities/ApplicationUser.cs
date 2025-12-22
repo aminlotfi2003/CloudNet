dff66deba07ec2d1ca77fe18d05e4c1e28ca0281
aminlotfi2003 <lotfiamin2003@gmail.com>
2025-12-22 16:02:23 +0330

    Add project files.

---
+++ src/CloudNet.WebFramework/Entities/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CloudNet.WebFramework.Entities;
+
+public class ApplicationUser : IdentityUser<Guid>
+{
+    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+    public DateTimeOffset? LastModifiedAt { get; set; }
+    
+    public long StorageLimit { get; set; }
+    public long StorageUsed { get; set; }
+
+    public ICollection<File> Files { get; set; } = new HashSet<File>();
+    public ICollection<Folder> Folders { get; set; } = new HashSet<Folder>();
+}
